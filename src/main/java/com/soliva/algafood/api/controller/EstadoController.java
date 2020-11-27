275775fe5e7a623b544022881d8253536e269bd9
Michel Soliva <michelsolivadev@gmail.com>
2020-11-27 17:09:21 -0300

    Implementando recurso de estado

---
+++ src/main/java/com/soliva/algafood/api/controller/EstadoController.java
@@ -0,0 +1,24 @@
+package com.soliva.algafood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soliva.algafood.domain.model.Estado;
+import com.soliva.algafood.domain.repository.EstadoRepository;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@GetMapping
+	private List<Estado> listar() {
+		return estadoRepository.listar();
+	}
+}
