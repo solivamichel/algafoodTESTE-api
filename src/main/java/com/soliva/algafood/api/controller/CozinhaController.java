5cb52c3fbe6addcf69c1c1a93cc704de4a46c093
Michel Soliva <michelsolivadev@gmail.com>
2020-11-28 12:54:33 -0300

    Implementando a camada de domain services

--- src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.soliva.algafood.domain.model.Cozinha;
 import com.soliva.algafood.domain.repository.CozinhaRepository;
+import com.soliva.algafood.domain.service.CadastroCozinhaService;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -26,6 +27,9 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
+	
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
 
 	@GetMapping
 	public List<Cozinha> listar() {
@@ -41,11 +45,11 @@ public class CozinhaController {
 		}
 		return ResponseEntity.notFound().build();
 	}
-
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-		return cozinhaRepository.salvar(cozinha);
+		return cadastroCozinha.salvar(cozinha);
 	}
 
 	@PutMapping("/{cozinhaId}")
---
+++ src/main/java/com/soliva/algafood/domain/service/CadastroCozinhaService.java
@@ -0,0 +1,18 @@
+package com.soliva.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.soliva.algafood.domain.model.Cozinha;
+import com.soliva.algafood.domain.repository.CozinhaRepository;
+
+@Service
+public class CadastroCozinhaService {
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public Cozinha salvar( Cozinha cozinha ) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+}
