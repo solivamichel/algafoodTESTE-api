893ccf76412b95e4bffa23566f18903e8a63b53d
Michel Soliva <michelsolivadev@gmail.com>
2020-11-28 00:41:02 -0300

    Corrigindo Status HTPP para resource inexistente

--- src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.soliva.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,12 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{id}")
-	public Cozinha buscar( @PathVariable Long id ) {
-		return cozinhaRepository.buscar(id);
+	public ResponseEntity<Cozinha> buscar( @PathVariable Long id ) {
+		Cozinha cozinha = cozinhaRepository.buscar(id);
+		
+		if( cozinha != null ) {
+			return ResponseEntity.ok(cozinha);
+		}
+			return ResponseEntity.notFound().build();
 	}
 }
