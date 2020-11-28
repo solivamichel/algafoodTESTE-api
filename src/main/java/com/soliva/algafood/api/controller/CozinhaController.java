acb35fcc03d92ae1a40e95e92a2c6e73253b6b00
Michel Soliva <michelsolivadev@gmail.com>
2020-11-28 13:32:33 -0300

    Modelando e implementando a consulta de recursos de restaurantes

--- src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.soliva.algafood.domain.exception.EntidadeEmUsoException;
+import com.soliva.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.soliva.algafood.domain.model.Cozinha;
 import com.soliva.algafood.domain.repository.CozinhaRepository;
 import com.soliva.algafood.domain.service.CadastroCozinhaService;
@@ -59,7 +60,7 @@ public class CozinhaController {
 		if (cozinhaAtual != null) {
 			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
 
-			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			cozinhaAtual = cadastroCozinha.salvar(cozinhaAtual);
 			return ResponseEntity.ok(cozinhaAtual);
 		}
 
@@ -68,19 +69,16 @@ public class CozinhaController {
 
 	@DeleteMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
-		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
 
 		try {
+			cadastroCozinha.excluir(cozinhaId);
 			
-			if (cozinha != null) {
-				cozinhaRepository.remover(cozinha);
-
-				return ResponseEntity.noContent().build();
-			}
-
+			return ResponseEntity.noContent().build();
+			
+		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.notFound().build();
-
-		} catch (DataIntegrityViolationException e) {
+			
+		} catch (EntidadeEmUsoException e) {
 			return ResponseEntity.status(HttpStatus.CONFLICT).build();
 		}
 	}
--- src/main/java/com/soliva/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/soliva/algafood/domain/service/CadastroCozinhaService.java
@@ -1,8 +1,12 @@
 package com.soliva.algafood.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.soliva.algafood.domain.exception.EntidadeEmUsoException;
+import com.soliva.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.soliva.algafood.domain.model.Cozinha;
 import com.soliva.algafood.domain.repository.CozinhaRepository;
 
@@ -14,5 +18,17 @@ public class CadastroCozinhaService {
 
 	public Cozinha salvar( Cozinha cozinha ) {
 		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	public void excluir( Long cozinhaId ) {
+		try {
+			cozinhaRepository.remover( cozinhaId );
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(String.format("Não existe um cadastro de cozinha com código %d", cozinhaId));
+			
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format("Cozinha de código %d não pode ser removida, pois está em uso.", cozinhaId));
+		}
 	}
 }
