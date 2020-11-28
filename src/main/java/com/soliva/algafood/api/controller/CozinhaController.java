c58d12bfbe8b9527dc219fa3b4409f14ad76e0b9
Michel Soliva <michelsolivadev@gmail.com>
2020-11-28 01:10:18 -0300

    Modelando e implementando a atualização de recursos com PUT

--- src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/soliva/algafood/api/controller/CozinhaController.java
@@ -2,12 +2,14 @@ package com.soliva.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,7 +42,48 @@ public class CozinhaController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public void adicionar( @RequestBody Cozinha cozinha ) {
-		cozinhaRepository.salvar(cozinha);
+	public Cozinha adicionar( @RequestBody Cozinha cozinha ) {
+		return cozinhaRepository.salvar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar( @PathVariable Long cozinhaId, @RequestBody Cozinha cozinha ) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(cozinhaId);
+		
+		if( cozinhaAtual != null ) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+			
+			cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
